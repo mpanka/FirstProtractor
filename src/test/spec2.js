60efb392fed7ed5037a4f4c6af2a22d8f1e98a88
mpanka <mervay.panka@gmail.com>
2021-02-09 14:09:19 +0100

    Put the elements into variables

--- src/test/spec2.js
+++ src/test/spec2.js
@@ -1,9 +1,15 @@
 describe('Protractor Demo App', function() {
+
+    var firstNumber = element(by.model('first'));
+    var secondNumber = element(by.model('second'));
+    var goButton = element(by.id('gobutton'));
+    var latestResult = element(by.binding('latest'));
+
     it('should add one and two', function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
-        element(by.model('first')).sendKeys(1);
-        element(by.model('second')).sendKeys(2);
-        element(by.id('gobutton')).click();
-        expect(element(by.binding('latest')).getText()).toEqual('5');
+        firstNumber.sendKeys(1);
+        secondNumber.sendKeys(2);
+        goButton.click();
+        expect(latestResult.getText()).toEqual('3');
     });
 });
