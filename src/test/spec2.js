024047c5727ebdb91ad5f7d60e858faa11b40390
mpanka <mervay.panka@gmail.com>
2021-02-09 14:28:08 +0100

    Add more tests/ specs

--- src/test/spec2.js
+++ src/test/spec2.js
@@ -9,10 +9,26 @@ describe('Protractor Demo App', function() {
         browser.get('http://juliemr.github.io/protractor-demo/');
     });
 
+    it('should have a title', function() {
+        expect(browser.getTitle()).toEqual('Super Calculator');
+    });
+
     it('should add one and two', function() {
         firstNumber.sendKeys(1);
         secondNumber.sendKeys(2);
         goButton.click();
         expect(latestResult.getText()).toEqual('3');
+    });
+
+    it('should add four and six', function() {
+        firstNumber.sendKeys(4);
+        secondNumber.sendKeys(6);
+        goButton.click();
+        expect(latestResult.getText()).toEqual('10');
+    });
+
+    it('should read the value from an input', function() {
+        firstNumber.sendKeys(1);
+        expect(firstNumber.getAttribute('value')).toEqual('1');
     });
 });
